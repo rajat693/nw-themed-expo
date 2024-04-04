1920f0eb51b7f2c33f8fbc0fb52152d476cf03bd
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-04-04 13:29:20 +0530

    feat: nativewind setup done

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import { GluestackUIProvider, Text, Box } from '@gluestack-ui/themed';
-import { config } from '@gluestack-ui/config'; // Optional if you want to use default theme
+import { GluestackUIProvider,Box } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
+import "./global.css";
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <GluestackUIProvider config={config}>
-      <Box justifyContent="center" alignItems="center" flex={1} >
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
-    </GluestackUIProvider>
+    // <GluestackUIProvider config={config}>
+    //   <Box justifyContent="center" alignItems="center" flex={1} >
+    //     <Text>Open up App.js to start working on your app!</Text>
+    //   </Box>
+    // </GluestackUIProvider>
+
+    <View className='bg-red-500 flex-1 items-center justify-center'>
+      <Text>jkjfbvfdkjv</Text>
+    </View>
   );
 }
